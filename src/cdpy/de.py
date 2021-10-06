93438820b1e6b2232615ab1703eca3cae0f35b49
Webster Mudge <wmudge@cloudera.com>
2021-10-06 14:52:17 -0400

    Update signatures with types Implement CPU and Memory requests for create_vc function Fix invalid return field for list_services
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- src/cdpy/de.py
+++ src/cdpy/de.py
@@ -7,7 +7,7 @@ class CdpyDe(CdpSdkBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def describe_vc(self, cluster_id, vc_id):
+    def describe_vc(self, cluster_id: str, vc_id: str):
         return self.sdk.call(
             svc='de', func='describe_vc', ret_field='vc', squelch=[
                 Squelch('NOT_FOUND'), Squelch('INVALID_ARGUMENT')
@@ -16,7 +16,7 @@ class CdpyDe(CdpSdkBase):
             vcId=vc_id
         )
 
-    def list_vcs(self, cluster_id):
+    def list_vcs(self, cluster_id: str):
          return self.sdk.call(
             svc='de', func='list_vcs', ret_field='vcs', squelch=[
                 Squelch(value='NOT_FOUND', default=list()),
@@ -25,20 +25,21 @@ class CdpyDe(CdpSdkBase):
             clusterId=cluster_id
         )
 
-    def create_vc(self, name, cluster_id):
+    def create_vc(self, name: str, cluster_id: str, cpu_requests: int = 4, mem_requests: int = 4):
         return self.sdk.call(
-            svc='de', func='create_vc', ret_field='vc',
-            vcName=name,
-            clusterId=cluster_id
-            # TODO: need to define cpu and mem requests
+            svc='de', func='create_vc', ret_field='Vc',
+            name=name,
+            clusterId=cluster_id,
+            cpuRequests=str(cpu_requests),
+            memoryRequests=str(mem_requests)
         )
 
-    def delete_vc(self, cluster_id, vc_id):
+    def delete_vc(self, cluster_id: str, vc_id: str):
         return self.sdk.call(
             svc='de', func='delete_vc', squelch=[Squelch('NOT_FOUND')], clusterId=cluster_id, vcId = vc_id
         )
 
-    def describe_service(self, cluster_id):
+    def describe_service(self, cluster_id: str):
         return self.sdk.call(
             svc='de', func='describe_service', ret_field='service', squelch=[
                 Squelch('NOT_FOUND'), Squelch('INVALID_ARGUMENT')
@@ -48,7 +49,7 @@ class CdpyDe(CdpSdkBase):
 
     def list_services(self):
         return self.sdk.call(
-            svc='de', func='list_services', ret_field='clusters', squelch=[
+            svc='de', func='list_services', ret_field='services', squelch=[
                 Squelch(value='NOT_FOUND', default=list())
             ]
         )
@@ -60,17 +61,17 @@ class CdpyDe(CdpSdkBase):
             ],
             name=name,
             env=env,
-            instance-type=instance_type,
-            minimum-instances=min_instances,
-            maximum-instances=max_instances
+            instanceType=instance_type,
+            minimumInstances=min_instances,
+            maximumInstances=max_instances
         )
 
-    def disable_service(self, cluster_id):
+    def disable_service(self, cluster_id: str):
         return self.sdk.call(
-            svc='de', func='disable_service', ret_field='status', squelch=[Squelch('NOT_FOUND')], cluster-id=cluster_id
+            svc='de', func='disable_service', ret_field='status', squelch=[Squelch('NOT_FOUND')], clusterId=cluster_id
         )
 
-    def get_kubeconfig(self, cluster_id):
+    def get_kubeconfig(self, cluster_id: str):
         return self.sdk.call(
-            svc='de', func='get_kubeconfig', ret_field='kubeconfig', squelch=[Squelch('NOT_FOUND')], cluster-id=cluster_id
+            svc='de', func='get_kubeconfig', ret_field='kubeconfig', squelch=[Squelch('NOT_FOUND')], clusterId=cluster_id
         )
